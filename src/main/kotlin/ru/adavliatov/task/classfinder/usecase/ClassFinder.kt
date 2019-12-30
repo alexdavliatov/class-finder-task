21279ce903370f170198ab55fc9b1a871f6d9f9b
adavliatov <alexander.davliatov@gmail.com>
2019-12-30 17:14:47 +0300

    Refactoring performed.

--- src/main/kotlin/ru/adavliatov/task/classfinder/usecase/ClassFinder.kt
+++ src/main/kotlin/ru/adavliatov/task/classfinder/usecase/ClassFinder.kt
@@ -8,17 +8,6 @@ import ru.adavliatov.task.classfinder.usecase.StrategiesExtensions.toItemsWithSt
 import ru.adavliatov.task.classfinder.usecase.handler.Handler
 import ru.adavliatov.task.classfinder.usecase.handler.HandlerExtensions.invoke
 
-//notes:
-//+package names ignored
-//+order alphabetically
-//
-//source: visitor (could be file, filesystem, ...) - sequence
-//
-//strategies:
-//validation: add filters
-//0. common => Upper case in the right order + lower case
-//1. lowercase only => case insensitive
-//2. endsWith if end space present
 class ClassFinder(private val config: Config) {
 
     fun find(input: Item, items: Sequence<Item>): Sequence<Item> = items
